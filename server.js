e4cc79a7a54fc62e51961776245a7867c4ee7b80
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-25 13:27:28 -0700

    Add rendering to fruit show page

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs');
 });
 
+app.get('/fruits/:fruitId', async (req, res) => {
+    const foundFruit = await Fruit.findById(req.params.fruitId);
+    res.render('fruits/show.ejs', { fruit: foundFruit });
+});
+
 app.post('/fruits', async (req, res) => {
     if (req.body.isReadyToEat === 'on') {
         req.body.isReadyToEat = true;
